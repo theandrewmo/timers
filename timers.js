73bfbbf0aedbd63173d7ff3ce626aa880ceb6095
Andrew Mo <106348010+theandrewmo@users.noreply.github.com>
2023-01-30 13:58:09 -0700

    adding index file and the modified timer.js

--- timers.js
+++ timers.js
@@ -1,14 +1,34 @@
-function countdown(num) {
-    let id = setInterval(function() {
-        if (num > 1) {
-            num = num - 1;
-            console.log(num)
-        }
-        else {
-            console.log('DONE!')
-            clearInterval(id);
-        }
-    }, 1000)
+const countBtn = document.getElementById('startCountdown')
+const countNum = document.getElementById('count')
+const randBtn = document.getElementById('randomGame')
+const finalNum = document.getElementById('finalNum')
+const finalCountDown = document.getElementById('finalCountDown')
+
+countBtn.addEventListener('click', countdown)
+randBtn.addEventListener('click', randomGame)
+
+function countdown(e) {
+    e.preventDefault();
+    finalCountDown.innerHTML = '';
+    let num = countNum.value;
+    if(!isNaN(parseInt(num))) {
+        let id = setInterval(function() {
+            if (num > 1) {
+                num = num - 1;
+                finalCountDown.append(` ${num} `)
+                console.log(num)
+            }
+            else {
+                console.log('DONE!')
+                finalCountDown.append('Done!')
+                clearInterval(id);
+            }
+        }, 1000)
+    }   
+    else {
+        alert('Not a number')
+    }
+    countNum.value = ''
 }
 
 
@@ -20,6 +40,7 @@ function randomGame() {
         counter = counter + 1;
         if(rand > .75) {
             console.log(counter);
+            finalNum.innerHTML = `This round took ${counter} seconds to find a random number between 0 and 1 that was >.75`;
             clearInterval(id);
         }
     },1000)
